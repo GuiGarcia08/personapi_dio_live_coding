edfa84145e0da00b6e661f8a7040c3a1d96e779d
Guilherme Garcia <guilherme.garcia@accurate.com.br>
2021-08-20 16:04:39 -0300

    Developed person created operation

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,15 +1,23 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.Repository.PersonRepository;
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/people")
 public class PersonController {
 
-	@GetMapping
-	public String getBook() {
-		return "API Test!";
+	@Autowired
+	PersonService service;
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public MessageResponseDTO cretePerson(@RequestBody Person person) {
+		return service.cretePerson(person);
 	}
 }
