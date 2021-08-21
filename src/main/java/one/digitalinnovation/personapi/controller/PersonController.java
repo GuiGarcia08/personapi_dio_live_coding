f34065fc385afcf91c6605e183b0cda9b2bfedbb
Guilherme Garcia <guilherme.garcia@accurate.com.br>
2021-08-21 14:59:43 -0300

    Developed list all mathod

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/people")
@@ -21,5 +22,10 @@ public class PersonController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public MessageResponseDTO cretePerson(@RequestBody @Valid PersonDTO personDTO) {
 		return service.cretePerson(personDTO);
+	}
+
+	@GetMapping
+	public List<PersonDTO> listAll(){
+		return service.listAll();
 	}
 }
